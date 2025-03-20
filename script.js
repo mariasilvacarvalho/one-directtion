b13517e82eee16f9174d964ee42f8dd7732d9e39
mariasilvacarvalho <146203552+mariasilvacarvalho@users.noreply.github.com>
2025-03-20 11:26:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+       botaoDeAcessibilidade.setAttribute ('aria-expanded', !botaoSelecionado); 
 
-    })
+     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
 
@@ -24,4 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+
+
 })
